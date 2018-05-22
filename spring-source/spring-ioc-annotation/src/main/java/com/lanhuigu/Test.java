17fd57b6bf491ca9e233cb205fb50e2b0978215a
yihonglei <yihonglei@baofeng.com>
2018-05-22 15:53:35 +0800

    spring源码分析IOC容器具体实现XmlBeanFactory

---
+++ spring-source/spring-ioc-annotation/src/main/java/com/lanhuigu/Test.java
@@ -0,0 +1,32 @@
+package com.lanhuigu;
+
+import javafx.application.Application;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.io.Resource;
+
+
+/**
+ * @author yihonglei
+ * @ClassName: Test
+ * @Package: com.lanhuigu
+ * @date 2018/5/22 11:47
+ * @version 1.0.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        /*BeanFactory;
+        FactoryBean;
+        XmlBeanFactory;
+        DefaultListableBeanFactory;
+        ApplicationContext;
+        ConfigurableApplicationContext;
+        Resource;
+        BeanDefinition;*/
+    }
+}
