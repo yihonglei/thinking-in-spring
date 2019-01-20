74147859b4fb3feb86cb636afdcf2ca932e42cc7
yihonglei <yihonglei@bcpeople.com>
2019-01-20 12:52:10 +0800

    代码重命名含义调整

--- spring-base/spring/src/main/java/com/lanhuigu/spring/proxy/compare/ProxyTest.java
+++ spring-base/spring/src/main/java/com/lanhuigu/spring/proxy/compare/ProxyTest.java
@@ -3,7 +3,7 @@ package com.lanhuigu.spring.proxy.compare;
 /**
  * 代理模式[[ 客户端--》代理对象--》目标对象 ]]
  */
-public class Client {
+public class ProxyTest {
     public static void main(String[] args) {
         System.out.println("**********************CGLibProxy**********************");
         CGLibProxy cgLibProxy = new CGLibProxy();
